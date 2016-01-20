69be492e9ada9479617d5a0fbd392b1af9ce76b2
ChubbyHunteR <luka.strizic@hotmail.com>
2016-01-20 17:48:43 +0100

    UI changes
    Signed-off-by: ChubbyHunteR <luka.strizic@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     qInstallMessageHandler((QtMessageHandler)&GroundStation::logHandler);
     GroundStation w;
-    w.setFixedSize(1300, 690);
+    w.setFixedSize(1350, 690);
     w.show();
 
     return a.exec();
